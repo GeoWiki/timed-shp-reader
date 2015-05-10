17041d624378ba62722631064ce34a9a3c0b4edf
Victor Sergienko <singalen@gmail.com>
2015-05-10 11:23:58 +0300

    Earthbyte fixed back

---
+++ gplates-data/shp_to_json.py
@@ -0,0 +1,73 @@
+#!/bin/python3
+import shapefile
+import json
+
+
+def _swap_pairs(pair_seq):
+    return [(b, a) for (a, b) in pair_seq]
+
+def _b_to_str(s):
+    if isinstance(s, bytes):
+        return s.decode().strip()
+    return s
+
+
+class ShapefileWrapper:
+    def __init__(self, shapefile_path):
+        self.sf = shapefile.Reader(shapefile_path)
+
+    def __to_record_dict(self, record):
+        """
+        :param record: (0-based) list of field values for self.sf.fields.
+        :return: A dict for the record: {field_name: field_value}
+        """
+        record_fields = {f[0]: _b_to_str(r) for (f,r) in zip(self.sf.fields, ['dummy'] + record)}
+        return record_fields
+
+    def record_iter(self):
+        """
+        :return: An iterator of ShapeRecord-s for the shapefile.
+        """
+        return [
+            self.__to_record_dict(r)
+            for r in self.sf.iterRecords()
+        ]
+
+
+class EarthbyteShapeRecord:
+    def __init__(self, fields_dict):
+        print(fields_dict)
+        self.plate_id = int(fields_dict['PLATEID1'])
+        self.type = _b_to_str(fields_dict['TYPE'])
+        self.from_age = float(fields_dict['FROMAGE'])
+        self.to_age = float(fields_dict['TOAGE'])
+        self.name = _b_to_str(fields_dict['NAME'])
+        self.description = _b_to_str(fields_dict['DESCR'])
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+def earthbyte_json_iter(sf_wrapper):
+    records = [EarthbyteShapeRecord(r) for r in sf_wrapper.record_iter()]
+    return [
+        {'FeatureType': record.type, 'FeatureBegin': record.from_age, 'FeatureEnd': record.to_age,
+         'FeatureName': record.name, 'FeaturePoints': _swap_pairs(shape.points)}
+        for record, shape in zip(records, sf_wrapper.sf.iterShapes())
+    ]
+
+
+def test(self):
+    swapped = _swap_pairs([(1,2), (3,4)])
+    assert swapped == [(2,1), (4,3)]
+
+
+if __name__ == '__main__':
+    # test()
+    shape_file_path = "./earthbyte/ContinentalPolygons/Shapefile/Seton_etal_ESR2012_ContinentalPolygons_2012.1.shp"
+    # shape_file_path = "./cshapes/cshapes.shp"
+    wrapper = ShapefileWrapper(shape_file_path)
+    print('record_iter', '\n'.join([str(r) for r in earthbyte_json_iter(wrapper)]))
+
+    with open('shp.json', 'w') as outfile:
+        json.dump(earthbyte_json_iter(wrapper), outfile, indent=2)
